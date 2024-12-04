fc9ca5e693246698e705f7972cb2735667284bf1
Stefan Roelofs <s.roelofs@brinkbv.com>
2024-12-04 19:42:09 +0100

    End of section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { authGuard } from './_guards/auth.guard';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -17,7 +20,10 @@ export const routes: Routes = [
             { path: 'members/:id', component: MemberDetailComponent },
             { path: 'lists', component: ListsComponent },
             { path: 'messages', component: MessagesComponent },
-            { path: '**', component: HomeComponent, pathMatch: 'full' },
         ],
     },
+    { path: 'errors', component: TestErrorsComponent, pathMatch: 'full' },
+    { path: 'not-found', component: NotFoundComponent, pathMatch: 'full' },
+    { path: 'server-error', component: ServerErrorComponent, pathMatch: 'full' },
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
